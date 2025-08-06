5529ad5876c1052782c926f27c22d8c34e7b1adb
GiovanniD1694 <giovanni.diaz@ingetic.cl>
2025-08-06 01:48:53 -0400

    Fix para que funcione el docker local y compose

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = Number(process.env.PORT) || 4000;
+  await app.listen(port, '0.0.0.0');
+  const logger = new Logger('bootstrap');
+  logger.log(`Listening on ${await app.getUrl()}`);
 }
-bootstrap();
+
+bootstrap().catch((e) => console.log(`Error al iniciar la aplicacion: ${e}`));
